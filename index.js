ff1599246dbff5633838a962a7baf2b1b79d2a1a
oFelipeBorba <ofelipeborba@gmail.com>
2022-07-07 18:33:27 -0300

    Tarefa 6 da semana 2

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
- var valor1 = window.prompt("Descubra se o valor digitado é par ou ímpar:");
- 
- if (valor1 %2 == 0) {
-  document.write("<h3>O seu resultado é par</h3>");
- } else {
-  document.write("<h3>O seu resultado é ímpar</h3>");
- }
+var peso = window.prompt("Digite o seu peso:");
+var altura = window.prompt(
+  "Digite a sua altura(Utilize um ponto '.' para realizar a separação das casas decimais):"
+);
+var imc = parseFloat(peso) / (parseFloat(altura) * parseFloat(altura));
+console.log(imc);
+if (imc >= 25) {
+  window.alert("Você está acima do peso, procure um médico");
+} else if (imc > 18.5 && imc < 24.9) {
+  document.write("PARABÉNS!! Você está no peso ideal");
+} else {
+  document.write("Você está abaixo do peso, procure um médico.");
+}
