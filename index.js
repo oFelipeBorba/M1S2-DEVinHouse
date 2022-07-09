8831ab0d92f1c8b5214956b10478f4d6cb5d3a45
oFelipeBorba <ofelipeborba@gmail.com>
2022-07-09 12:32:17 -0300

    Tarefa 9 da semana 2 - divergente do enunciado

--- index.js
+++ index.js
@@ -1,15 +1,76 @@
-// Utilizando estrutura de repetição for realize a tabuada do número 5 e exibe em tela da seguinte forma:
+// Você foi contratado por um supermercado para catalogar a compra de uma cliente.
+//Assim que a aplicação começa deverá ser perguntado em um prompt qual produto o cliente deseja comprar,
+//só serão aceitas as seguintes respostas:
 
-// 5 x 0 = 0
-// 5 x 1 = 5
-// 5 x 2 = 10
-// ....
-// 5 x 10 = 50
+// (1) Hortifruti
+// (2) Laticínios
+// (3) Carnes
+// (4) Peixes
+// (5) Aves
+// (6) Fechar pedido
+// Após fazer a seleção do produto pergunte a quantidade de itens que o cliente irá comprar.
+// Apenas se o cliente selecionar a opção 6 é que a pergunta deverá parar de ser repetida.
+// Ao selecionar a opção 6 imprima o produto que o cliente pegou em maior quantidade.
 
-//variavel i será o multiplicador e ao mesmo tempo o que limita meu for
-var i; 
-var atualResult = 0;
-for ( i = 0; i<=10 ; i++) {
-    atualResult = (5*parseFloat(i));
-    document.write('5 x '+parseFloat(i)+' = '+atualResult+'</br>');
-}
+var i = 0;
+var itemInicio = [
+  "Hortifruti",
+  "",
+  "Laticíneos",
+  "",
+  "Carnes",
+  "",
+  "Peixes",
+  "",
+  "Aves",
+  "",
+  "Fechar o pedido",
+];
+var item = [
+  "Hortifruti",
+  "",
+  "Laticíneos",
+  "",
+  "Carnes",
+  "",
+  "Peixes",
+  "",
+  "Aves",
+  "",
+  "Fechar o pedido",
+];
+do {
+  if (i == 10) {
+    item[i] = window.alert("Seu pedido será fechado.");
+    i++;
+  } else {
+    item[i] = window.prompt(
+      "Você gostaria de comprar algum item de " +
+        item[i] +
+        ". Responda apenas com: 'sim' ou 'não'"
+    );
+    if (item[i] == "sim" && item[i] != "não") {
+      i++;
+      item[i] = window.prompt("Quantos itens você gostaria?");
+      i++;
+    } else if (item[i] != "sim" && item[i] == "não") {
+      i++;
+      item[i] = 0;
+      i++;
+    } else if (item[i] != "sim" && item[i] != "não") {
+      window.alert("Por favor, responda apenas com 'sim' ou 'não'");
+      item[i] = itemInicio[i];
+    }
+  }
+} while (i < 11);
+console.log("Gostaria de hortifruti:" + item[0]);
+console.log("Quantidade de itens em hortifruti:" + item[1]);
+console.log("Gostaria de laticínios:" + item[2]);
+console.log("Quantidade de itens em laticínios:" + item[3]);
+console.log("Gostaria de carnes:" + item[4]);
+console.log("Quantidade de itens em carnes:" + item[5]);
+console.log("Gostaria de peixes:" + item[6]);
+console.log("Quantidade de itens em peixes:" + item[7]);
+console.log("Gostaria de aves:" + item[8]);
+console.log("Quantidade de itens em aves:" + item[9]);
+console.log(item[10]);
